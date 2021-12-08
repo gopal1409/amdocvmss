87da3129c54e85a4d12fb2ca2d34492185ac75af
gopal <gopal1409@gmail.com>
2021-12-08 15:26:49 +0530

    terraform remote state

--- c1.provider.tf
+++ c1.provider.tf
@@ -15,6 +15,12 @@ terraform {
       version = ">= 3.0"
     }     
   }
+  backend "azurerm" {
+    resource_group_name = terraform-storage-rg
+    #storage_account_name = terraformstategd
+    container_name = "tfstatestatefiles"
+    key = "project-1.tfstate"
+  }
 }
 
 # Provider Block
